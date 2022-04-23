018c3ff5b568b484b5e210cb34e353378be93493
ChauPhucNguyen <cdn9680@mavs.uta.edu>
2022-04-23 18:03:33 -0500

    first push

--- app/src/main/java/com/example/maverickbazaar/MainActivity.java
+++ app/src/main/java/com/example/maverickbazaar/MainActivity.java
@@ -4,11 +4,75 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvFirstName, tvLastName, tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser == null) {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        tvFirstName = findViewById(R.id.tvFirstName);
+        tvLastName = findViewById(R.id.tvLastName);
+        tvEmail = findViewById(R.id.tvEmail);
+
+        Button btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutUser();
+            }
+        });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("users").child(currentUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userInfo user = snapshot.getValue(userInfo.class);
+                if (user != null) {
+                    tvFirstName.setText("First Name: " + user.firstName);
+                    tvLastName.setText("Last Name: " + user.lastName);
+                    tvEmail.setText("Email: " + user.email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    private void logoutUser() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/maverickbazaar/userInfo.java
@@ -0,0 +1,17 @@
+package com.example.maverickbazaar;
+
+
+public class userInfo {
+    public String firstName;
+    public String lastName;
+    public String email;
+
+    public userInfo() {
+    }
+
+    public userInfo (String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
