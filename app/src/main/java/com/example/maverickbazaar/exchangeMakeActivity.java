16f282a1eca00fb971da91eb5590be94731dbafb
Christopher Nguyen <christopher.nguyen10@mavs.uta.edu>
2022-04-24 18:07:08 -0500

    I'm just hoping this works honestly This is just trying to see if Android will run tbh

---
+++ app/src/main/java/com/example/maverickbazaar/exchangeMakeActivity.java
@@ -0,0 +1,84 @@
+package com.example.maverickbazaar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+
+
+public class exchangeMakeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase rootNode;
+    private DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exchange_make);
+
+
+
+        Button makeExchangeButton = findViewById(R.id.postExchangeButton);
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            finish();
+            return;
+        }
+
+        makeExchangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeExchange();
+            }
+        });
+    }
+
+    private void makeExchange() {
+
+        EditText headerInputEdit = findViewById(R.id.headerInput);
+        EditText bodyInputEdit = findViewById(R.id.bodyInput);
+
+        String headerInput = headerInputEdit.getText().toString();
+        String bodyInput = bodyInputEdit.getText().toString();
+
+
+        if (headerInput.isEmpty() || bodyInput.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        exchangeInfo exchange = new exchangeInfo(headerInput,bodyInput);
+        rootNode = FirebaseDatabase.getInstance("https://maverickbazaar-default-rtdb.firebaseio.com");
+
+        reference = rootNode.getReference("exchanges");
+        reference.child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid()).setValue(exchange);
+    }
+
+
+
+    private void showMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
