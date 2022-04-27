531ab08870d881bb48b5032f7516d28533f5c1e4
HummusMan <humayunhumza30@gmail.com>
2022-04-27 11:34:03 -0500

    user l

---
+++ app/src/main/java/com/example/search/listings.java
@@ -0,0 +1,27 @@
+package com.example.search;
+
+public class listings {
+    public String name, details,price;
+
+    public listings(){
+
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name=name;
+    }
+    public String getDetails(){
+        return details;
+    }
+    public void setDetails(){
+        this.details=details;
+    }
+    public String getPrice(){
+        return price;
+    }
+    public void setPrice(){
+        this.price=price;
+    }
+}
