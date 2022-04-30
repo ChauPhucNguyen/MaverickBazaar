f33ec2ea36ba5fe6bb5492d5dcfae9bcafdeffb6
Christopher Nguyen <christopher.nguyen10@mavs.uta.edu>
2022-04-30 15:27:02 -0500

    Changelog: Added Search Function for Exchanges
    TODO: Filter system, image inclusion, order history, make it look prettier

--- app/src/main/java/com/example/maverickbazaar/ExchangeList.java
+++ app/src/main/java/com/example/maverickbazaar/ExchangeList.java
@@ -2,6 +2,12 @@ package com.example.maverickbazaar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +18,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -21,6 +28,8 @@ public class ExchangeList extends AppCompatActivity {
     ArrayList<exchangeInfo> list;
     DatabaseReference databaseReference;
     RecyclerAdapterExchanges adapter;
+
+    EditText editText;
 
     @Override
     public void onBackPressed() {
@@ -41,6 +50,17 @@ public class ExchangeList extends AppCompatActivity {
         adapter = new RecyclerAdapterExchanges(this, list);
         recyclerView.setAdapter(adapter);
 
+        editText = findViewById(R.id.searchExchange);
+        Button searchButton = findViewById(R.id.exchangeSearchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final String searchText = editText.getText().toString();
+                search(searchText);
+            }
+        });
+
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -52,6 +72,34 @@ public class ExchangeList extends AppCompatActivity {
             }
 
 
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+    private void search(String s) {
+        Toast.makeText(getApplicationContext() , "Searching Exchange", Toast.LENGTH_SHORT).show();
+        Query query = databaseReference.orderByChild("header").startAt(s).endAt(s+"\uf8ff");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChildren()){
+                    list.clear();
+                    for(DataSnapshot dss: dataSnapshot.getChildren()){
+                        final exchangeInfo exchangesInfo = dss.getValue(exchangeInfo.class);
+                        list.add(exchangesInfo);
+                    }
+
+                    RecyclerAdapterExchanges adapter = new RecyclerAdapterExchanges(getApplicationContext(), list);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+            }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
